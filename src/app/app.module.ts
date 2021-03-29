ef7c57961038b218b450886fbf5cca603355ce29
BobDoland <aarongrau55@gmail.com>
2021-03-29 01:36:00 -0500

    Added angular components
    Added
    about us
    contact us
    sports
    header
    footer
    html interfaces
    
    and imported bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header/header.component';
 import { FooterComponent } from './footer/footer/footer.component';
 import { NotFoundComponent } from './pages/not-found/not-found/not-found.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { SportsComponent } from './sports/sports.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    SportsComponent
   ],
   imports: [
     BrowserModule,
