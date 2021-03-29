b77b567fed5314bbcab8fddd777ab7c496f697a9
Jacob Fanis <47834448+JacobFanis@users.noreply.github.com>
2021-03-29 12:32:19 -0500

    FinalFinal
    Final version of Admin site pre user verification

--- AdminFinal/routes/newsEdit.js
+++ AdminFinal/routes/newsEdit.js
@@ -1,25 +1,48 @@
 var express = require('express');
-//const Contact = require('../models/newsModel');
+const news = require('../models/newsModel');
 const mongoose = require('../models/mongo');
 var router = express.Router();
 
-function display(data, res){
-  
-  console.log(data);
-    res.render('newsEdit', {data: data});
-  
-}
-router.get('/', function (req, res) {
-  const con = mongoose.createConnection('mongodb://localhost/newsdb', {useNewUrlParser: true, useUnifiedTopology: true});
-  con.once('open', function(){
-    con.db.collection("newsmodels", function(err, collection){
-      collection.find({}).toArray(function(err, data){
-        display(data, res);
+function findUsers(res){
+    const con = mongoose.createConnection('mongodb://localhost/newsdb', {useNewUrlParser: true, useUnifiedTopology: true});
+    con.once('open', function(){
+      con.db.collection("newsmodels", function(err, collection){
+        collection.find({}).toArray(function(err, data){
+            console.log(data);
+            res.render('newsEdit', {data: data});
+        });
       });
     });
-  });
-
+}
+router.get('/', function (req, res) {
+    findUsers(res);
 });
+router.get('/delete/:id', function(req, res, next) {
+    console.log(req.params.id);
+    news.findByIdAndRemove( req.params.id.trim(), (err, status) => {
+        if(!err){
+            console.log("News Deleted");
+            console.log(status);
+            findUsers(res);
+        }
+        else{
+            res.json(err)
+        }
+    })
+  });
+  router.get('/edit/:id/:desc/:time/:title', function(req, res, next) {
+    console.log(req.params);
+    news.findByIdAndUpdate( req.params.id.trim(), {inputTitle: req.params.title, inputDesc: req.params.desc, inputTime: req.params.time}, (err, status) => {
+        if(!err){
+            console.log("News updated");
+            console.log(status);
+            findUsers(res);
+        }
+        else{
+            res.json(err)
+        }
+    })
+  });
 
 
 module.exports = router;
