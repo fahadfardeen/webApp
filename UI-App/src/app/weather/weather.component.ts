376d53c415e2964a9207388aa106af9540a1561d
BobDoland <aarongrau55@gmail.com>
2021-03-31 11:37:38 -0500

    weather finsihed
    pog

--- UI-App/src/app/weather/weather.component.ts
+++ UI-App/src/app/weather/weather.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../Service/weather.service';
+
 
 @Component({
   selector: 'app-weather',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  errorMessage: string;
+  cityName: string;
+  disabledForecastButton: boolean;
+  weatherForecastData: any;
+  cityinitail:string;
+  
+  constructor(private _weatherService: WeatherService) {     
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    
+    this.cityinitail = 'Bangalore'
+    this._weatherService.getWeatherForecast(this.cityinitail)
+          .subscribe(data => { this.weatherForecastData = data ,console.log(this.weatherForecastData)} , error => this.errorMessage = <any>error );
+    
+          this.onResetControls();
+
+  }
+
+  onSubmitDatabinding() {
+    console.log('Inside the two way', this.cityName);
+    this._weatherService.getWeatherForecast(this.cityName)
+          .subscribe(data => { this.weatherForecastData = data ,console.log(this.weatherForecastData)} , error => this.errorMessage = <any>error );
+    
+         
+     this.onResetControls();
+}
+  onSearchLocationWithEvent(event: Event) {
+    console.log('Control event value', (<HTMLInputElement>event.target).value);
+    this.cityName = (<HTMLInputElement>event.target).value;
+    this.disabledForecastButton = false;
+  }
+  onResetControls() {
+    //console.log(this.weatherForecastData);
+    //this.cityName = '';
+    this.disabledForecastButton = true;
   }
 
 }
